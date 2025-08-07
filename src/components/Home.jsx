168926c337d93183a4b0e1606bb690f7ac199fc1
divyanshu <divyanshusoni651@gmail.com>
2025-08-07 17:02:49 +0530

    commit images product change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ import About from './pages/about/About';
 import WhyChooseUs from './pages/whyChoose/WhyChooseUs';
 import FaqSection from './pages/faq/FaqSection';
 import Testimonials from './pages/Testimonial/Testimonials';
-import OrderProccess from './pages/step/OrderProccess';
 import Product from './pages/product/Product';
 import BestSellerBanner from './pages/bestseller/BestSellerBanner';
+import Gallery from './pages/gallery/Gallery';
 const Home = () => {
   return (
     <div>
@@ -20,8 +20,8 @@ const Home = () => {
  
   <BestSellerBanner />
   <WhyChooseUs />
-  {/* <Gallery /> */}
- <OrderProccess />
+  <Gallery />
+ {/* <OrderProccess /> */}
     <Testimonials />
     <FaqSection />
 	<Footer />
